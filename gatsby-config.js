e7ba12f63d58fb95a6e0fa1204d45ebf23b9d497
Restyled.io <commits@restyled.io>
2020-07-13 03:59:38 +0000

    Restyled by clang-format

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,75 +4,76 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 const netlifyCmsPaths = {
-  resolve: `gatsby-plugin-netlify-cms-paths`,
-  options: {
-    cmsConfig: `/static/admin/config.yml`,
+  resolve : `gatsby-plugin-netlify-cms-paths`,
+  options : {
+    cmsConfig : `/static/admin/config.yml`,
   },
 }
 
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+                        require("dotenv")
+                            .config({
+                              path : `.env.${process.env.NODE_ENV}`,
+                            })
 
 const settings = require("./src/util/site.json")
 
 module.exports = {
-  siteMetadata: settings.meta,
-  plugins: [
+  siteMetadata : settings.meta,
+  plugins : [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/static/assets/`,
-        name: `assets`,
+      resolve : `gatsby-source-filesystem`,
+      options : {
+        path : `${__dirname}/static/assets/`,
+        name : `assets`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/page-content/`,
-        name: `page-content`,
+      resolve : `gatsby-source-filesystem`,
+      options : {
+        path : `${__dirname}/src/page-content/`,
+        name : `page-content`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/component-content/`,
-        name: `component-content`,
+      resolve : `gatsby-source-filesystem`,
+      options : {
+        path : `${__dirname}/src/component-content/`,
+        name : `component-content`,
       },
     },
     {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.mdx`, `.md`],
-        gfm: true,
-        plugins: [
+      resolve : `gatsby-plugin-mdx`,
+      options : {
+        extensions : [ `.mdx`, `.md` ],
+        gfm : true,
+        plugins : [
           netlifyCmsPaths,
           `gatsby-remark-reading-time`,
           {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1024,
-              showCaptions: true,
-              linkImagesToOriginal: false,
-              tracedSVG: true,
-              loading: "lazy",
+            resolve : `gatsby-remark-images`,
+            options : {
+              maxWidth : 1024,
+              showCaptions : true,
+              linkImagesToOriginal : false,
+              tracedSVG : true,
+              loading : "lazy",
             },
           },
           `gatsby-remark-responsive-iframe`,
           {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              classPrefix: "language-",
-              inlineCodeMarker: null,
-              aliases: {},
-              showLineNumbers: false,
-              noInlineHighlight: false,
+            resolve : `gatsby-remark-prismjs`,
+            options : {
+              classPrefix : "language-",
+              inlineCodeMarker : null,
+              aliases : {},
+              showLineNumbers : false,
+              noInlineHighlight : false,
               // By default the HTML entities <>&'" are escaped.
               // Add additional HTML escapes by providing a mapping
               // of HTML entities and their escape value IE: { '}': '&#123;' }
-              escapeEntities: {},
+              escapeEntities : {},
             },
           },
         ],
@@ -82,31 +83,31 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: settings.ga,
+      resolve : `gatsby-plugin-google-analytics`,
+      options : {
+        trackingId : settings.ga,
       },
     },
     `gatsby-plugin-advanced-sitemap`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-        icon: `static/assets/MLogo.png`,
+      resolve : `gatsby-plugin-manifest`,
+      options : {
+        name : `Foundation`,
+        short_name : `Foundation`,
+        start_url : `/`,
+        background_color : `#f7f0eb`,
+        theme_color : `#a2466c`,
+        display : `standalone`,
+        icon : `static/assets/MLogo.png`,
       },
     },
     "gatsby-plugin-offline",
     {
-      resolve: `gatsby-source-github-api`,
-      options: {
-        token: process.env.GITHUB_API_TOKEN,
-        variables: {},
-        graphQLQuery: `
+      resolve : `gatsby-source-github-api`,
+      options : {
+        token : process.env.GITHUB_API_TOKEN,
+        variables : {},
+        graphQLQuery : `
         query {
           viewer {
             repositories(first: 20, orderBy: {field: CREATED_AT, direction: DESC}) {
--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -1,45 +1,42 @@
+import Octicon, {Law} from "@githubprimer/octicons-react"
+import {graphql, StaticQuery} from "gatsby"
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import Octicon, { Law } from "@githubprimer/octicons-react"
 import GitHubButton from "react-github-btn"
-import { ExternalLink } from "./ExternalLink"
 
-const RepositoryHeader = ({ repo }) => {
+import {ExternalLink} from "./ExternalLink"
+
+const RepositoryHeader =
+    ({repo}) => {
   return (
     <div
-      style={{
-        display: `flex`,
-        justifyContent: `space-between`,
-        alignItems: `center`,
-        fontSize: 14,
-      }}
-    >
-      <h4
-        style={{
-          display: `flex`,
-          justifyContent: `space-between`,
-          margin: 0,
-          fontSize: 20,
-        }}
-      >
-        <ExternalLink url={`https://github.com${repo.resourcePath}`}>
-          {repo.name}
-        </ExternalLink>
-      </h4>
-      <GitHubButton
-        href={`https://github.com${repo.resourcePath}`}
-        dataIcon="octicon-star"
-        dataSize="large"
+  style = {
+    {
+      display: `flex`, justifyContent: `space-between`, alignItems: `center`,
+          fontSize: 14,
+    }
+  } > < h4
+  style = {
+    {
+      display: `flex`, justifyContent: `space-between`, margin: 0, fontSize: 20,
+    }
+  } > <ExternalLink url = {`https://github.com${repo.resourcePath}`}>{
+          repo.name}</ExternalLink>
+      </h4>< GitHubButton
+  href = {`https://github.com${repo.resourcePath}`} dataIcon = "octicon-star"
+  dataSize = "large"
         ariaLabel="Star repo on GitHub"
       >
         Star
       </GitHubButton>
     </div>
   )
-}
+    }
 
-const FooterItem = ({ children }) => (
-  <span style={{ marginRight: 16, whiteSpace: "nowrap" }}>{children}</span>
+const FooterItem = ({children}) =>
+    (<span style = {
+      { marginRight: 16, whiteSpace: "nowrap" }
+    }>{children}<
+        /span>
 )
 
 const RepositoryFooter = ({ repo }) => {
@@ -63,55 +60,55 @@ const RepositoryFooter = ({ repo }) => {
                     width: 12,
                     backgroundColor: language.color,
                   }}
-                />{" "}
-                {language.name}
-              </FooterItem>
+                />{
+        " "} {
+        language.name}</FooterItem>
             )
           })}
-      </div>
-      {repo.licenseInfo && (
-        <FooterItem>
-          <Octicon icon={Law} /> {repo.licenseInfo.name}
-        </FooterItem>
-      )}
-      <FooterItem>Created: {createdAt}</FooterItem>
-    </div>
-  )
+      </div>{
+        repo.licenseInfo &&
+        (<FooterItem>
+         <Octicon icon =
+          { Law } /> {repo.licenseInfo.name}
+        </FooterItem>)}<FooterItem>
+         Created: {createdAt}</FooterItem>
+    </div>)
 }
 
-const RepositoryDescription = ({ repo }) => (
-  <div style={{ width: `75%` }}>
-    <p style={{ color: `#586069`, marginBottom: 0 }}>
-      {repo.description && repo.description}
+const RepositoryDescription = ({repo}) =>
+    (<div style = {
+      { width: `75%` }
+    }><p style = {
+      { color: `#586069`, marginBottom: 0 }
+    }>{repo.description && repo.description}
 
-      {repo.homepageUrl && (
-        <>
-          {" -"}{" "}
-          <ExternalLink url={repo.homepageUrl}>{repo.homepageUrl}</ExternalLink>
-        </>
-      )}
-    </p>
-  </div>
-)
+     {repo.homepageUrl &&
+      (<>{" -"} {" "}<ExternalLink url = {repo.homepageUrl}>{
+          repo.homepageUrl}</ExternalLink>
+        </>)}</p>
+  </div>)
 
-const Repository = ({ repo }) => {
+const Repository =
+    ({repo}) => {
   return (
     <div
       style={{
-        borderBottom: `1px solid #e1e4e8`,
-        padding: `0 1rem 1rem 1rem`,
+    borderBottom: `1px solid #e1e4e8`, padding: `0 1rem 1rem 1rem`,
         fontSize: 16,
       }}
     >
-      <RepositoryHeader repo={repo} />
+      <RepositoryHeader repo={
+    repo} />
       <RepositoryDescription repo={repo} />
-      <RepositoryFooter repo={repo} />
+      <RepositoryFooter repo={
+    repo} />
     </div>
   )
-}
+    }
 
-const RepositorySectionMaker = ({ data }) => (
-  <div className="grids smd-2 col-1">{data}</div>
+const RepositorySectionMaker = ({data}) =>
+    (<div className = "grids smd-2 col-1">{data}<
+        /div>
 )
 
 export const RepositoryList = () => {
@@ -150,12 +147,11 @@ export const RepositoryList = () => {
         const repos = data.githubData.data.viewer.repositories.nodes.map(
           repo => {
             return repo.name !== "msichterman" ? (
-              <Repository key={repo.id} repo={repo} />
-            ) : null
-          }
+              <Repository key={repo.id} repo={repo} />): null
+}
         )
         return <RepositorySectionMaker data={repos} />
       }}
     />
   )
-}
+        }
